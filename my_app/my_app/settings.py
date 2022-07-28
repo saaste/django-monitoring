9cf1bf82114b49279e63d11957f166bbac73d084
Mika Hiltunen <github@haaska.net>
2022-07-28 08:21:52 +0300

    Add database monitoring support

--- my_app/my_app/settings.py
+++ my_app/my_app/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'my_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django_prometheus.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
