9652b6dda35a46fc3d53bc0d43a3e2f38881781b
Mika Hiltunen <github@haaska.net>
2022-07-28 12:35:50 +0300

    fixup! Add model monitoring support

--- my_app/cats/models.py
+++ my_app/cats/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django_prometheus.models import ExportModelOperationsMixin
 
-class Cat(ExportModelOperationsMixin("dog"), models.Model):
+class Cat(ExportModelOperationsMixin("cat"), models.Model):
     name = models.CharField(max_length=128)
     breed = models.CharField(max_length=128)
     likes_to_drop_things = models.BooleanField()
