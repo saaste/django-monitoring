79447f6e7b8a82b25287ef4da940aca6bfe47c3f
Mika Hiltunen <github@haaska.net>
2022-07-28 12:11:06 +0300

    Separate app views

--- my_app/cats/urls.py
+++ my_app/cats/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'cats'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id>/', views.details, name='details')
